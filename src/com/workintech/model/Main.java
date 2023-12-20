05a3834d4dd0d7841c548704c29630cde0bdfb71
bidoluyazılım <bidoluyazlm@gmail.com>
2023-12-20 14:10:53 +0300

    java 3. etut dersi

---
+++ src/com/workintech/model/Main.java
@@ -0,0 +1,46 @@
+package com.workintech.model;
+
+public class Main {
+    public static void main(String[] args) {
+        //Person person2 = new Person();
+        Person person = new Person("Ali", "Veli", 30);
+        //person.firstName.charAt(0);
+        //String[] hobbies = new String[]{"piano", "chess"};
+        Person davut = new Person("Davut", "Gurlek",
+                29, 8000, false, new String[]{"piano", "chess"});
+
+
+
+        System.out.println("Salary: " + davut.salary);
+        System.out.println("Is Teen: " + davut.isTeen());
+        System.out.println("Hobbies: " + davut.getHobbies());
+        //System.out.println("Hobbies: " + Arrays.toString(dogancan.hobbies));
+        System.out.println(davut);
+        davut.salary = 1000;
+
+        Person john = new Person("John", "Doe", 20);
+        System.out.println("Firstname: " + john.getFirstName());
+        System.out.println("LastName: " + john.getLastName());
+        System.out.println("Age: " + john.getAge());
+
+        System.out.println("*********************************");
+        Wall wall = new Wall(5, 4);
+        System.out.println("Get width: " + wall.getWidth());
+        System.out.println("Get height: " + wall.getHeight());
+        System.out.println("Area: " + wall.getArea());
+
+        wall.setWidth(-5);
+        System.out.println("Get width: " + wall.getWidth());
+        wall.setHeight(-4);
+        System.out.println("Get height: " + wall.getHeight());
+
+        Wall wall2 = new Wall(-5, -4);
+        System.out.println("Get width: " + wall2.getWidth());
+        System.out.println("Get height: " + wall2.getHeight());
+        wall2.width = -20;
+
+        One one = new One('c');
+        one.One("test");
+        one.x = 10;
+    }
+}
